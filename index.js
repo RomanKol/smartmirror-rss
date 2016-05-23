fa1dd3c76e5256aec164c9a7d0c4d32657f29b02
RomanKol <kollatschny.roman@web.de>
2016-05-23 18:09:45 +0200

    renamed app to controller

--- index.js
+++ index.js
@@ -3,23 +3,22 @@
 var express = require('express');
 var app = express();
 
-var feed = require('./app/app');
+var controller = require('./controller');
 
-app.set('views', __dirname + '/app');
+app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
   let url = 'http://www.heise.de/newsticker/heise-atom.xml'
-  feed.get(url)
+  controller.get(url)
   .then((feed) => {
     res.render('view.jade', {
       title: 'RSS feed Reader',
       feed: feed
     });
-    console.dir(feed.entries[0]);
   });
 });
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!');
 });
