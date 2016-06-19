e23bfc319797843292b8b53f894d64e36fd20c20
RomanKol <kollatschny.roman@web.de>
2016-06-19 16:30:47 +0200

    updated framework

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 var controller = require('./app/controller');
 
@@ -9,13 +10,29 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  let url = 'http://www.heise.de/newsticker/heise-atom.xml'
-  controller.get({url: url})
-  .then((feed) => {
-    res.render('app/view.jade', {
-      data: feed,
+
+  let pckg = fs.readFileSync('package.json');
+  pckg = JSON.parse(pckg);
+
+  let params = {};
+  params.url = 'http://www.heise.de/newsticker/heise-atom.xml';
+  let widget = {};
+  widget._id = '1234567890';
+  widget.size = pckg.smartmirror.size[0];
+
+  controller.get(params)
+  .then((data) => {
+    res.render('./index.jade', {
+      data: data,
+      widget: widget,
     });
+  })
+  .catch((err) => {
+    console.dir(err);
+    res.status(500).send(err);
   });
+
+
 });
 
 app.listen(8080, function () {
